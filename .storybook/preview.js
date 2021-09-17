f221d5d75a231a4737bf3a48ec7c910704652cac
Troy <86826396+tsheed@users.noreply.github.com>
2021-09-17 13:08:41 +0100

    Amended preview file.

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -13,14 +13,14 @@ export const decorators = [
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
-  a11y: {
-    element: '#root',
-    manual: false,
-  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+  a11y: {
+    element: '#root',
+    manual: false,
+  },
+};
